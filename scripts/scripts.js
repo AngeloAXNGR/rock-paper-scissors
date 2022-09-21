a29e9103f36eeee4930f576f693d734a3bea7207
LuhxEn <axngrsantos@gmail.com>
2022-09-21 12:08:23 +0800

    Add getComputerChoice function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const choices = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice(choices){
+  let index = Math.floor(Math.random(0) * choices.length);
+  let computerChoice = choices[index];
+  return computerChoice;
+}
+
+// let test = getComputerChoice(choices);
+// console.log(test);
