ffa7b7d6615b32cc8d1952acda4767a1db6d5923
LuhxEn <axngrsantos@gmail.com>
2022-09-21 12:39:19 +0800

    Add most functionalities of the Game I created a function called "game()" which allows the players to play tic-tac-toe against a computer. There are still some features that needs to be added such as a message displaying the winner of that round and how it was won as well as their current score of that round.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,5 +6,62 @@ function getComputerChoice(choices){
   return computerChoice;
 }
 
+// let playerInput = prompt('Select your weapon!');
+// let computerInput = getComputerChoice(choices);
+
+// console.log("Player: " + playerInput);
+// console.log("Computer: " + computerInput);
+
+let playerScore = 0;
+let computerScore = 0;
+
+let rounds = 5;
+
+
+function playGame(playerSelection, computerSelection){
+  if(playerSelection == "rock" && computerSelection == "scissors"){
+    return playerScore++;
+    
+  }else if(playerSelection == "rock" && computerSelection == "paper"){
+    return computerScore++
+
+  }else if(playerSelection == "paper" && computerSelection == "rock"){
+    return playerScore++;
+
+  }else if(playerSelection == "paper" && computerSelection == "scissors"){
+    return computerScore++;
+
+  }else if(playerSelection == "scissors" && computerSelection == "paper"){
+    return playerScore++;
+
+  }else if(playerSelection == "scissors" && computerSelection == "rock"){
+    return computerScore++
+
+  }else{
+    rounds++;
+  }
+}
+
+function game(){
+  for(let i = 1; i <= rounds; i++){
+    let playerInput = prompt('Select your weapon!');
+    let computerInput = getComputerChoice(choices);
+    playGame(playerInput, computerInput);
+    console.log("Player: " + playerInput);
+    console.log("Computer: " + computerInput);
+  }
+
+  if(playerScore > computerScore){
+    console.log("Player Wins!");
+  }else{
+    console.log("Computer Wins!");
+  }
+}
+// playGame(playerInput, computerInput);
+
+game();
+
+console.log('Player Score: ' + playerScore);
+console.log('Computer Score: ' + computerScore);
 // let test = getComputerChoice(choices);
 // console.log(test);
