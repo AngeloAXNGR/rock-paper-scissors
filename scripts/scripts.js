42019508657784b62915ee0f1c90bb35fa1e9255
LuhxEn <axngrsantos@gmail.com>
2022-09-26 09:55:57 +0800

    Convert console based rock-paper-scissors game into an Interface.
    Major changes were made where scores are no longer displayed in the console tab
    but directly on the page itself. I also included a message that displays who
    won for a specific round in addition to announcing who the winner is once the game ends.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,57 +8,82 @@ function getComputerChoice(choices){
 
 
 
-let scores = [0,0]
-let playerScore = scores[0]
-let computerScore = scores[1]
+
+let playerScore = 0;
+let computerScore = 0;
 
 // Best of 9 or First to Five
-let rounds = 9;
+// let rounds = 9;
 
+
+// Get three buttons
+const btns = document.querySelectorAll('button');
+let playerTally = document.querySelector('.player-score');
+let computerTally = document.querySelector('.computer-score');
+
+btns.forEach((btn) =>{
+  btn.addEventListener('click', () =>{
+    playerInput = btn.textContent.toLowerCase();
+    computerInput = getComputerChoice(choices);
+    playRound(playerInput, computerInput);
+    getScore(playerScore, computerScore);
+    computerTally.textContent = computerScore;
+    playerTally.textContent = playerScore;
+  });
+});
+
+
+
+const finalResult = document.createElement('h1');
+finalResult.setAttribute('style', 'text-align:center;')
+
+
+function getScore(playerScore, computerScore){
+  if(playerScore == 5){
+    finalResult.textContent = `You Win!`;
+    roundResult.textContent = ''
+    document.body.appendChild(finalResult);
+  }else if(computerScore == 5){
+    finalResult.textContent = `You Lost!`;
+    roundResult.textContent = ''
+    document.body.appendChild(finalResult);
+  }  
+}
+
+
+
+
+let roundResult = document.querySelector('#round-result');
+roundResult.textContent = '';
+roundResult.style.textAlign = 'Center';
 
 function playRound(playerSelection, computerSelection){
-
   if(playerSelection == "rock" && computerSelection == "scissors"){
-    return playerScore++;
+    roundResult.textContent = 'You Win! Rock Beats Scissors!';
     
+    return playerScore++;
+
   }else if(playerSelection == "rock" && computerSelection == "paper"){
+    roundResult.textContent = 'You Lose! Paper Beats Rock!';
     return computerScore++
 
   }else if(playerSelection == "paper" && computerSelection == "rock"){
+    roundResult.textContent = 'You Win! Paper Beats Rock!';
     return playerScore++;
 
   }else if(playerSelection == "paper" && computerSelection == "scissors"){
+    roundResult.textContent = 'You Lose! Scissors Beats Paper!';
     return computerScore++;
 
   }else if(playerSelection == "scissors" && computerSelection == "paper"){
+    roundResult.textContent = 'You Win! Scissors Beats PAper!';
     return playerScore++;
 
   }else if(playerSelection == "scissors" && computerSelection == "rock"){
+    roundResult.textContent = 'You Lose! Rock Beats Scissors!';
     return computerScore++
   }else{
-    rounds++;
+    roundResult.textContent = "It's a Tie!";
   }
 }
 
-function game(){
-  for(let i = 0; i < rounds; i++){
-    let playerInput = prompt('Select Your Weapon: ');
-    let computerInput = getComputerChoice(choices);
-    playRound(playerInput.toLowerCase(), computerInput);
-    console.log(`Player: ${playerScore} || Computer ${computerScore}`);
-
-    // Whoever reaches 5 first will win and if it does, the round ends, otherwise (else) the game will continue.
-    if(playerScore == 5){
-      console.log("Player Wins!");
-      break;
-    }else if(computerScore == 5){
-      console.log("Computer Wins...")
-      break;
-    }else{
-      continue;
-    }
-  }
-
-}
-
-game();
